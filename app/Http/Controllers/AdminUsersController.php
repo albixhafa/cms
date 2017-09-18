78eb67c18796c2091c8471ffb56707a0869aae36
Albi <xhafa.albi@gmail.com>
2017-09-18 20:46:38 +0200

    Users page working without photos

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Requests\UserRequest;
+Use App\User;
+Use App\Role;
 
 class AdminUsersController extends Controller
 {
@@ -16,7 +19,9 @@ class AdminUsersController extends Controller
     public function index()
     {
         //
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+        
     }
 
     /**
@@ -26,7 +31,8 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        return view('admin.users.create');
+        $roles = Role::lists('name', 'id')->all();
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -35,9 +41,11 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        //
+        User::create($request->all());
+        return redirect('/admin/users');
+        //return $request->all();
     }
 
     /**
---
+++ test.html
@@ -0,0 +1,12 @@
+{!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+
+\t\t<div class="form-group">
+\t\t\t{!! Form::label('name', 'Name: ') !!}
+\t\t\t{!! Form::text('name', null, ['class'=>'form-control']) !!}
+\t\t</div>
+
+\t\t<div class="form-group">
+\t\t\t{!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+\t\t</div>
+\t
+\t{!! Form::close() !!}
